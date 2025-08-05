7e8eaecf12f046c770e50ebd40f62a86351413a9
vankaspr <msvankaspr@gmail.com>
2025-08-05 20:22:28 +0500

    🔧 black formatter + 📌 add logging in main file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
+import logging
+import sys
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QTime
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, QTimeEdit,
-                             QLabel, QPushButton, QHBoxLayout)
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QWidget,
+    QVBoxLayout,
+    QTimeEdit,
+    QLabel,
+    QPushButton,
+    QHBoxLayout,
+)
 from utilits.timer import TimerLogic
 from utilits.load_stylesheet import load_stylesheet
 from utilits.load_fonts import load_pixelify_font
+from libs.logger import setup_logging, get_logger
+
+setup_logging()
+logger = get_logger(__name__)
+
 
 class TimerWindow(QMainWindow):
     def __init__(self):
@@ -28,7 +43,7 @@ class TimerWindow(QMainWindow):
         # input time
         self.time_edit = QTimeEdit()
         self.time_edit.setDisplayFormat("HH:mm:ss")
-        self.time_edit.setTime(QTime(0,1,0)) #00.01.00 default
+        self.time_edit.setTime(QTime(0, 1, 0))  # 00.01.00 default
 
         # Отображение времени
         self.time_label = QLabel("00:00:00")
@@ -38,7 +53,6 @@ class TimerWindow(QMainWindow):
         self.start_btn = QPushButton("start")
         self.pause_btn = QPushButton("pause")
         self.reset_btn = QPushButton("reset")
-
 
         # Горизонтальный layout для кнопок
         btn_layout = QHBoxLayout()
@@ -58,9 +72,8 @@ class TimerWindow(QMainWindow):
         self.pause_btn.clicked.connect(self.pause_timer)
         self.reset_btn.clicked.connect(self.reset_timer)
 
-
     def start_timer(self):
-        """start the timer with the selected time """
+        """start the timer with the selected time"""
         time = self.time_edit.time()
         self.timer.set_time(time.hour(), time.minute(), time.second())
         self.timer.start()
@@ -79,7 +92,6 @@ class TimerWindow(QMainWindow):
         self.start_btn.setEnabled(True)
         self.pause_btn.setEnabled(False)
 
-
     def update_display(self, time_str):
         """Update time display"""
         self.time_label.setText(time_str)
@@ -91,16 +103,28 @@ class TimerWindow(QMainWindow):
         self.pause_btn.setEnabled(False)
 
 
+def main():
+    try:
+        logger.info("Starting application")
+
+        app = QApplication(sys.argv)
+
+        logger.debug("QApplication created")
+
+        font_family = load_pixelify_font()
+        if font_family:
+            app.setFont(QFont(font_family, 24))
+
+        window = TimerWindow()
+        window.show()
+        logger.info("Main window displayed")
+
+        sys.exit(app.exec_())
+
+    except Exception as e:
+        logger.critical("Application failed", exc_info=True)
+        raise Exception(f"error {e}")
+
+
 if __name__ == "__main__":
-    import sys
-    from PyQt5.QtWidgets import  QApplication
-
-    app = QApplication(sys.argv)
-
-    font_family = load_pixelify_font()
-    if font_family:
-        app.setFont(QFont(font_family, 24))
-
-    window = TimerWindow()
-    window.show()
-    sys.exit(app.exec_())
+    main()
