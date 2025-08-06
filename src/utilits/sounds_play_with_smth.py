97390d969f0d794dbe45519164da84733ca52bfc
vankaspr <msvankaspr@gmail.com>
2025-08-06 15:19:30 +0500

    📌 create helper for sound play

---
+++ src/utilits/sounds_play_with_smth.py
@@ -0,0 +1,21 @@
+from typing import Callable
+
+from utilits import SoundPlayer
+
+
+def sound_wrapper(
+    sound_player: SoundPlayer,
+    sound_name: str,
+    target_func: Callable[[], None],
+) -> Callable[[], None]:
+    """Wrapper to play sound before executing a function"""
+
+    def wrapper():
+        try:
+            sound_player.play(sound_name)
+        except Exception as e:
+            print(f"Error sound play: {e}")
+        finally:
+            target_func()
+
+    return wrapper
