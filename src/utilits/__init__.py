c672b9db5d2b0a3d5afdcdde3175ff3750bee2f0
vankaspr <msvankaspr@gmail.com>
2025-08-06 15:19:51 +0500

    📌 add new helper and class in init file

--- src/utilits/__init__.py
+++ src/utilits/__init__.py
@@ -1,9 +1,14 @@
 from .load_stylesheet import load_stylesheet
 from .load_fonts import load_pixelify_font
 from .timer import TimerLogic
+from .sounds import SoundPlayer
+from .sounds_play_with_smth import sound_wrapper
+
 
 __all__ = [
     "load_pixelify_font",
     "load_stylesheet",
     "TimerLogic",
+    "SoundPlayer",
+    "sound_wrapper",
 ]
