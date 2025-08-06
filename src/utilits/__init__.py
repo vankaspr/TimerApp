52526dc261bd0c99fac7ae5970e7e6eb6093cd85
vankaspr <msvankaspr@gmail.com>
2025-08-06 17:47:49 +0500

    📌 add new helper to init

--- src/utilits/__init__.py
+++ src/utilits/__init__.py
@@ -4,6 +4,7 @@ from .timer import TimerLogic
 from .play_sound import SoundPlayer
 from .button_sounds import sound_wrapper
 from .QTimeEdit_sounds import add_unified_sound_to_time_edit
+from .random_image_loader import get_random_image
 
 
 __all__ = [
@@ -13,4 +14,5 @@ __all__ = [
     "SoundPlayer",
     "sound_wrapper",
     "add_unified_sound_to_time_edit",
+    "get_random_image",
 ]
